f2594bd639135404164199b30d6ee10d5d16e884
asimonia <alex.simonian@gmail.com>
2016-10-18 15:02:11 -0500

    Initial commit

---
+++ strings.js
@@ -0,0 +1,18 @@
+"use strict";
+
+function reversal() {
+
+}
+
+function alphabits() {
+
+}
+
+function palindrome() {
+
+}
+
+var testString = "";
+reversal(testString);
+alphabits(testString);
+palindrome(testString);
